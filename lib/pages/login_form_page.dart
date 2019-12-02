a69a840286088e6de5d227136f77898ce0411218
Azam <azam@Azams-MacBook-Air.local>
2019-12-02 20:46:27 +0800

    test

--- lib/pages/login_form_page.dart
+++ lib/pages/login_form_page.dart
@@ -42,19 +42,13 @@ class _LoginFormPageState extends State<LoginFormPage> {
                 ),
               ),
             ),
-            Expanded(
-              child: FlatButton(
-                onPressed: () {_bloc.dispatch(GotoSplashPage());},
-                child: Text("Click me to get back there!"),
-              ),
-            )
-            // Align(
-            //     alignment: Alignment.bottomCenter,
-            //     child: BlocProvider(
-            //       builder: (context) =>
-            //           BlocProvider.of<PageManagerBloc>(context),
-            //       child: LoginFormContainer(),
-            //     )),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: BlocProvider(
+                  builder: (context) =>
+                      BlocProvider.of<PageManagerBloc>(context),
+                  child: LoginFormContainer(),
+                )),
           ],
         ),
       ]),
@@ -62,105 +56,105 @@ class _LoginFormPageState extends State<LoginFormPage> {
   }
 }
 
-// class LoginFormContainer extends StatefulWidget {
-//   @override
-//   _LoginFormContainerState createState() => _LoginFormContainerState();
-// }
+class LoginFormContainer extends StatefulWidget {
+  @override
+  _LoginFormContainerState createState() => _LoginFormContainerState();
+}
 
-// class _LoginFormContainerState extends State<LoginFormContainer> {
-//   final TextEditingController _emailController = new TextEditingController();
-//   final TextEditingController _passController = new TextEditingController();
-//   String _pass = "";
-//   String _email = "";
+class _LoginFormContainerState extends State<LoginFormContainer> {
+  final TextEditingController _emailController = new TextEditingController();
+  final TextEditingController _passController = new TextEditingController();
+  String _pass = "";
+  String _email = "";
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//         height: 450,
-//         decoration: BoxDecoration(
-//             borderRadius: BorderRadius.only(
-//                 topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-//             color: Colors.white),
-//         child: Column(
-//           children: <Widget>[
-//             Padding(
-//               padding: EdgeInsets.only(top: 30),
-//             ),
-//             Padding(
-//               padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-//               child: TextField(
-//                 controller: _emailController,
-//                 decoration: InputDecoration(hintText: "Email"),
-//                 keyboardType: TextInputType.emailAddress,
-//                 onChanged: (value) {
-//                   _email = value;
-//                 },
-//               ),
-//             ),
-//             Padding(
-//               padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-//               child: TextField(
-//                 obscureText: true,
-//                 controller: _passController,
-//                 decoration: InputDecoration(hintText: "Password"),
-//                 onChanged: (value) {
-//                   _pass = value;
-//                 },
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.only(top: 20),
-//               child: signinButton(),
-//             ),
-//             Padding(padding: EdgeInsets.only(top: 40)),
-//             Text(
-//               "Need Support?",
-//               style: TextStyle(color: Color(0xFF07877D), fontSize: 18),
-//             ),
-//             Padding(padding: EdgeInsets.only(top: 40)),
-//             Padding(
-//               padding: const EdgeInsets.only(left: 15),
-//               child: Align(
-//                 alignment: Alignment.bottomLeft,
-//                 child: IconButton(
-//                   icon: Icon(
-//                     Icons.arrow_back,
-//                     size: 40,
-//                     color: Colors.grey,
-//                   ),
-//                   onPressed: () {
-//                     BlocProvider.of<PageManagerBloc>(context)
-//                         .dispatch(GotoLoginOrSignupPage());
-//                   },
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ));
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 450,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+            color: Colors.white),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 30),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(hintText: "Email"),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  _email = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: TextField(
+                obscureText: true,
+                controller: _passController,
+                decoration: InputDecoration(hintText: "Password"),
+                onChanged: (value) {
+                  _pass = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: signinButton(),
+            ),
+            Padding(padding: EdgeInsets.only(top: 40)),
+            Text(
+              "Need Support?",
+              style: TextStyle(color: Color(0xFF07877D), fontSize: 18),
+            ),
+            Padding(padding: EdgeInsets.only(top: 40)),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    size: 40,
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    BlocProvider.of<PageManagerBloc>(context)
+                        .dispatch(GotoLoginOrSignupPage());
+                  },
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
 
-//   Widget signinButton() {
-//     return Container(
-//       width: double.infinity,
-//       height: 55,
-//       padding: EdgeInsets.symmetric(horizontal: 30),
-//       child: FlatButton(
-//         onPressed: () {
-//           print("Login Button Pressed!");
-//           print("Email: $_email, Password: $_pass");
-//         },
-//         child: Text(
-//           "Sign In",
-//           style: TextStyle(
-//             color: Colors.white,
-//             fontSize: 18,
-//           ),
-//         ),
-//         shape: RoundedRectangleBorder(
-//           borderRadius: BorderRadius.circular(20),
-//         ),
-//         color: Color(0xFF07877D),
-//       ),
-//     );
-//   }
-// }
+  Widget signinButton() {
+    return Container(
+      width: double.infinity,
+      height: 55,
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: FlatButton(
+        onPressed: () {
+          print("Login Button Pressed!");
+          print("Email: $_email, Password: $_pass");
+        },
+        child: Text(
+          "Sign In",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        color: Color(0xFF07877D),
+      ),
+    );
+  }
+}
